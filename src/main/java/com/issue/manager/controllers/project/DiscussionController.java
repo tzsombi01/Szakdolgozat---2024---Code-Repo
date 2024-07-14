c8b7a86d153cdc0e62890d3e1603059cb940d419
tzsombi01 <toreky.zsombor@gmail.com>
2024-07-14 13:20:31 +0200

    feature(discussion-endpoints): Discussion endpoints created

--- src/main/java/com/issue/manager/controllers/project/DiscussionController.java
+++ src/main/java/com/issue/manager/controllers/project/DiscussionController.java
@@ -1,10 +1,48 @@
 package com.issue.manager.controllers.project;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.issue.manager.inputs.project.DiscussionInput;
+import com.issue.manager.models.core.QueryOptions;
+import com.issue.manager.models.project.Discussion;
+import com.issue.manager.services.project.DiscussionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/discussions")
+@RequiredArgsConstructor
 public class DiscussionController {
 
+    private final DiscussionService discussionService;
+
+    @PostMapping("/get")
+    @ResponseStatus(HttpStatus.OK)
+    public Page<Discussion> getDiscussions(@RequestBody QueryOptions queryOptions) {
+        return discussionService.getDiscussions(queryOptions);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Discussion getDiscussion(@PathVariable String id) {
+        return discussionService.getDiscussion(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Discussion createDiscussion(@RequestBody DiscussionInput discussionInput) {
+        return discussionService.createDiscussion(discussionInput);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Discussion editDiscussion(@PathVariable String id, @RequestBody DiscussionInput discussionInput) {
+        return discussionService.editDiscussion(id, discussionInput);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Discussion deleteDiscussion(@PathVariable String id) {
+        return discussionService.deleteDiscussion(id);
+    }
 }
--- src/main/java/com/issue/manager/services/project/DiscussionService.java
+++ src/main/java/com/issue/manager/services/project/DiscussionService.java
@@ -1,11 +1,55 @@
 package com.issue.manager.services.project;
 
+import com.issue.manager.inputs.project.DiscussionInput;
+import com.issue.manager.models.project.Discussion;
+import com.issue.manager.repositories.project.DiscussionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
 @RequiredArgsConstructor
 public class DiscussionService {
+
+    private final DiscussionRepository discussionRepository;
+
+    public Page<Discussion> getDiscussions() {
+        return new PageImpl<>(discussionRepository.findAll());
+    }
+
+    public Discussion getDiscussion(String id) {
+        Discussion discussion = discussionRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Discussion was not found by id " + id));
+
+        return discussion;
+    }
+
+    public Discussion createDiscussion(DiscussionInput discussionInput) {
+        Discussion discussion = discussionInput.toModel();
+
+        return discussionRepository.save(discussion);
+    }
+
+    public Discussion editDiscussion(String id, DiscussionInput discussionInput) {
+        Discussion discussion = discussionRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Discussion was not found by id " + id));
+
+        Discussion editedDiscussion = discussionInput.toModel(discussion);
+
+        discussionRepository.save(editedDiscussion);
+
+        return discussion;
+    }
+
+    public Discussion deleteDiscussion(String id) {
+        Discussion discussion = discussionRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Discussion was not found by id " + id));
+
+        discussionRepository.deleteById(id);
+
+        return discussion;
+    }
 }
