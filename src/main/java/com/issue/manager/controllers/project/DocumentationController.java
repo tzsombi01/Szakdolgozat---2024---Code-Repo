7e902f843420c37c78262dc0b28023f324ecd39c
tzsombi01 <toreky.zsombor@gmail.com>
2024-07-14 12:40:46 +0200

    feature(documentation-endpoints): Documentation endpoints created

--- src/main/java/com/issue/manager/controllers/project/DocumentationController.java
+++ src/main/java/com/issue/manager/controllers/project/DocumentationController.java
@@ -1,10 +1,48 @@
 package com.issue.manager.controllers.project;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.issue.manager.inputs.project.DocumentationInput;
+import com.issue.manager.models.core.QueryOptions;
+import com.issue.manager.models.project.Documentation;
+import com.issue.manager.services.project.DocumentationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/documentations")
+@RequiredArgsConstructor
 public class DocumentationController {
 
+    private final DocumentationService documentationService;
+
+    @PostMapping("/get")
+    @ResponseStatus(HttpStatus.OK)
+    public Page<Documentation> getDocumentations(@RequestBody QueryOptions queryOptions) {
+        return documentationService.getDocumentations(queryOptions);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Documentation getDocumentation(@PathVariable String id) {
+        return documentationService.getDocumentation(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Documentation createDocumentation(@RequestBody DocumentationInput documentationInput) {
+        return documentationService.createDocumentation(documentationInput);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Documentation editDocumentation(@PathVariable String id, @RequestBody DocumentationInput documentationInput) {
+        return documentationService.editDocumentation(id, documentationInput);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Documentation deleteDocumentation(@PathVariable String id) {
+        return documentationService.deleteDocumentation(id);
+    }
 }
--- src/main/java/com/issue/manager/services/project/DocumentationService.java
+++ src/main/java/com/issue/manager/services/project/DocumentationService.java
@@ -1,11 +1,60 @@
 package com.issue.manager.services.project;
 
+import com.issue.manager.inputs.project.DocumentationInput;
+import com.issue.manager.models.core.QueryOptions;
+import com.issue.manager.models.project.Documentation;
+import com.issue.manager.repositories.project.DocumentationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Log4j2
 @RequiredArgsConstructor
 public class DocumentationService {
+
+    private final DocumentationRepository documentationRepository;
+
+    public Page<Documentation> getDocumentations(QueryOptions queryOptions) {
+        List<Documentation> all = documentationRepository.findAll();
+
+        return new PageImpl<>(all);
+    }
+
+    public Documentation getDocumentation(String id) {
+        Documentation documentation = documentationRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Documentation was not found by id " + id));
+
+        return documentation;
+    }
+
+    public Documentation createDocumentation(DocumentationInput documentationInput) {
+        Documentation documentation = documentationInput.toModel();
+
+        return documentationRepository.save(documentation);
+    }
+
+    public Documentation editDocumentation(String id, DocumentationInput documentationInput) {
+        Documentation documentation = documentationRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Documentation was not found by id " + id));
+
+        Documentation editedDocumentation = documentationInput.toModel(documentation);
+
+        documentationRepository.save(editedDocumentation);
+
+        return documentation;
+    }
+
+    public Documentation deleteDocumentation(String id) {
+        Documentation documentation = documentationRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Documentation was not found by id " + id));
+
+        documentationRepository.deleteById(id);
+
+        return documentation;
+    }
 }
