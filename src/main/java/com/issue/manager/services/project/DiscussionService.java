ff4bc1327c20b7ce0292d5f0fa41242390b728bd
tzsombi01 <toreky.zsombor@gmail.com>
2024-09-08 19:12:28 +0200

    fix(discussion): DiscussionService getAll and create implemented

--- src/main/java/com/issue/manager/services/project/DiscussionService.java
+++ src/main/java/com/issue/manager/services/project/DiscussionService.java
@@ -1,13 +1,15 @@
 package com.issue.manager.services.project;
 
 import com.issue.manager.inputs.project.DiscussionInput;
+import com.issue.manager.models.base.User;
+import com.issue.manager.models.core.Filter;
 import com.issue.manager.models.core.QueryOptions;
 import com.issue.manager.models.project.Discussion;
 import com.issue.manager.repositories.project.DiscussionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.*;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,7 +20,23 @@ public class DiscussionService {
     private final DiscussionRepository discussionRepository;
 
     public Page<Discussion> getDiscussions(QueryOptions queryOptions) {
-        return new PageImpl<>(discussionRepository.findAll());
+        Discussion exampleDiscussion = new Discussion();
+        ExampleMatcher matcher = ExampleMatcher.matching();
+
+        if (queryOptions.getFilters() != null) {
+            for (Filter filter : queryOptions.getFilters()) {
+                if ("project".equals(filter.getField())) {
+                    exampleDiscussion.setProject((String) filter.getValue());
+                    matcher = matcher.withMatcher("project", ExampleMatcher.GenericPropertyMatchers.exact());
+                }
+            }
+        }
+
+        Example<Discussion> example = Example.of(exampleDiscussion, matcher);
+
+        Pageable pageable = PageRequest.of(queryOptions.getSkip(), queryOptions.getTake() > 0 ? queryOptions.getTake() : 10);
+
+        return discussionRepository.findAll(example, pageable);
     }
 
     public Discussion getDiscussion(String id) {
@@ -30,6 +48,9 @@ public class DiscussionService {
 
     public Discussion createDiscussion(DiscussionInput discussionInput) {
         Discussion discussion = discussionInput.toModel();
+
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        discussion.setCreator(user.getId());
 
         return discussionRepository.save(discussion);
     }
