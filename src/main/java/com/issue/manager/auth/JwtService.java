d83e6470f9402c89766351f9d547de60bea6d67c
tzsombi01 <toreky.zsombor@gmail.com>
2024-11-16 16:01:05 +0100

    feature(ticketcontroller-tests): Added Controller Test

--- src/main/java/com/issue/manager/auth/JwtService.java
+++ src/main/java/com/issue/manager/auth/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,8 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = System.getenv("JWT_SECRET_KEY");
+    @Value("${jwt.secret.key}")
+    private String SECRET_KEY;
 
     private static final int TOKEN_VALIDITY_24_HOURS = 1000 * 60 * 24;
 
---
+++ src/test/java/com/issue/manager/controllers/TicketControllerTest.java
@@ -0,0 +1,98 @@
+package com.issue.manager.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.issue.manager.auth.JwtService;
+import com.issue.manager.inputs.project.TicketInput;
+import com.issue.manager.models.base.User;
+import com.issue.manager.models.project.Project;
+import com.issue.manager.models.project.Ticket;
+import com.issue.manager.repositories.base.UserRepository;
+import com.issue.manager.repositories.project.ProjectRepository;
+import com.issue.manager.repositories.project.TicketRepository;
+import com.issue.manager.utils.EmailService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class TicketControllerTest {
+
+    public static final String baseTicketEndpointUrl = "/api/tickets";
+
+    @Autowired
+    private ProjectRepository projectRepository;
+    @Autowired
+    private TicketRepository ticketRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JwtService jwtService;
+
+    @MockBean
+    private EmailService emailService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAll();
+        projectRepository.deleteAll();
+        ticketRepository.deleteAll();
+    }
+
+    @Test
+    void itShould_createTicket() throws Exception {
+        User user = new User();
+        user.setEmail("test@gmail.com");
+        user.setUserName("test");
+
+        User savedUser = userRepository.save(user);
+
+
+        Project project = new Project("testUrl", "test", List.of(), List.of());
+        Project savedProject = projectRepository.save(project);
+
+        TicketInput ticketInput = new TicketInput();
+        ticketInput.setProject(savedProject.getId());
+        ticketInput.setName("Ticket 1");
+
+        // Mock the SecurityContext and Authentication
+        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+        Authentication authentication = Mockito.mock(Authentication.class);
+
+        Mockito.when(authentication.getPrincipal()).thenReturn(user);
+        Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+
+        String ticketInputJson = new ObjectMapper().writeValueAsString(ticketInput);
+
+        String token = jwtService.generateToken(savedUser);
+        mockMvc.perform(post(baseTicketEndpointUrl)
+                        .header("authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(ticketInputJson))
+                .andExpect(status().isCreated());
+
+        List<Ticket> all = ticketRepository.findAll();
+        assertThat(all.size()).isPositive();
+    }
+}
